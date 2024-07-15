f47d3c2e297d604a2be89571a27e8b2d21279f77
Dimplektech <dimplekaware@gmail.com>
2024-07-15 17:17:17 +0100

    Quiz Game

---
+++ question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+new_quest = Question("jkdkd", "False")        
+print(new_quest.text)
